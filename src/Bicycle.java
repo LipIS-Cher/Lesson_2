9acdeb5d4a183bb873a2cdff92095d1e8faec5c6
Ivan <lipatow.ivan2016@yandex.ru>
2023-08-19 16:20:59 +0300

    Init

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,9 +1,26 @@
-public class Bicycle {
+public class Bicycle implements Transport {
 
-    public String modelName;
-    public int wheelsCount;
+    private String modelName;
+    private int wheelsCount;
 
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    public Bicycle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
     }
 }
---
+++ src/Transport.java
@@ -0,0 +1,14 @@
+public interface Transport {
+     static void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    static void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+    static void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+
+}
